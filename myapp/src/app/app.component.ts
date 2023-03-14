0c3a6be4c7f3efe76134710c07604a1cbe53b1d2
OgabekToshpolatov <ogabektoshpolatov2002@gmail.com>
2023-03-14 12:09:36 +0500

    Added Add_Product Form

--- myapp/src/app/app.component.ts
+++ myapp/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { DialogComponent } from './dialog/dialog.component';
+import {MatDialog, MAT_DIALOG_DATA} from '@angular/material/dialog';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private dialog:MatDialog){}
   title = 'myapp';
+  openDialog() {
+    this.dialog.open(DialogComponent, {
+       width:"30%"
+    });
+  }
 }
+  export class ButtonOverviewExample {}
